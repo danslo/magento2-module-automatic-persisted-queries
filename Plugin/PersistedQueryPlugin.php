93fbed2afd0d3801a3b969cbe9530c0311b009b0
Anael Ollier <aollier@clever-age.com>
2023-08-22 15:49:46 +0200

    Fix codestyle

--- Plugin/PersistedQueryPlugin.php
+++ Plugin/PersistedQueryPlugin.php
@@ -72,9 +72,10 @@ class PersistedQueryPlugin
 
         if ($persistedQueryHash && hash('sha256', $data['query']) !== $persistedQueryHash) {
             $result->setHttpResponseCode($request->isPost()
-                ? $this->scopeConfig->getValue('apq/general/invalid_sha_http_code_post', ScopeInterface::SCOPE_STORES) ?? 500
-                : $this->scopeConfig->getValue('apq/general/invalid_sha_http_code_get', ScopeInterface::SCOPE_STORES) ?? 400
-            );
+                ? $this->scopeConfig->getValue('apq/general/invalid_sha_http_code_post', ScopeInterface::SCOPE_STORES)
+                    ?? 500
+                : $this->scopeConfig->getValue('apq/general/invalid_sha_http_code_get', ScopeInterface::SCOPE_STORES)
+                    ?? 400);
             $result->setBody('provided sha does not match query');
             return $result;
         }
@@ -95,9 +96,10 @@ class PersistedQueryPlugin
 
         // apollo-server responds with 400 for GET and 500 for POST when no query is found
         $jsonResult->setHttpResponseCode($request->isPost()
-            ? $this->scopeConfig->getValue('apq/general/query_not_found_http_code_post', ScopeInterface::SCOPE_STORES) ?? 500
-            : $this->scopeConfig->getValue('apq/general/query_not_found_http_code_get', ScopeInterface::SCOPE_STORES) ?? 400
-        );
+            ? $this->scopeConfig->getValue('apq/general/query_not_found_http_code_post', ScopeInterface::SCOPE_STORES)
+            ?? 500
+            : $this->scopeConfig->getValue('apq/general/query_not_found_http_code_get', ScopeInterface::SCOPE_STORES)
+            ?? 400);
         $jsonResult->setData([
             'errors' => [
                 [
